b3df575d0863e573a51e05217c9f6b82370f9af5
stybik <bikrocksbiswas@gmail.com>
2020-12-25 13:51:49 +0530

    set row and coloum to zero if the element is zero in a matrix

---
+++ 1_array/13_zero_matrix.py
@@ -0,0 +1,34 @@
+r = 3
+c = 4
+
+def changeMatrix(mat):
+    row = [0] * r
+    col = [0] * c
+
+    for i in range(0, r):
+        for j in range(0, c):
+            if (mat[i][j] == 0):
+                row[i] = 0
+                col[i] = 0
+
+    for i in range(0, r):
+        for j in range(0, c):
+            if(row[i] == 0 or col[j] == 0):
+                mat[i][j] = 0
+    
+
+def printMatrix(mat):
+    for i in range(0, r):
+        for j in range(0, c):
+            print(mat[i][j], end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    mat = [ [1, 0, 0, 1], 
+            [0, 0, 1, 0], 
+            [0, 0, 0, 0] ]
+    print(f"Input Matrix: {mat}")
+    changeMatrix(mat)
+    print(f"Modified Matrix: ")
+    printMatrix(mat)
