3264270dc565c34187f7c08bdc2bd6499efe1ce6
stybik <bikram.biswas@coverfoxmail.com>
2021-04-06 12:52:54 +0530

    refactor

--- 1_array/2_reverse_except_special_characters.py
+++ 1_array/2_reverse_except_special_characters.py
@@ -1,20 +1,37 @@
+
+def isAlphabet(x):
+	return x.isalpha()
+
 def reverse(string):
-	LIST = list(string)
+	LIST = toList(string)
 	r = len(LIST) - 1
 	l = 0
 
 	while(l < r):
-		if not LIST[l].isalpha():
+		if not isAlphabet(LIST[l]):
 			l += 1
-		elif not LIST[r].isalpha():
+		elif not isAlphabet(LIST[r]):
 			r -= 1
 
 		else:
-			LIST[l], LIST[r] = LIST[l], LIST[r]
+			LIST[l], LIST[r] = swap(LIST[l], LIST[r])
 			l += 1
 			r -= 1
 
-	return ''.join(LIST)
+	return toString(LIST)
+
+
+def toList(string):
+	List = []
+	for i in string:
+		List.append(i)
+	return List
+
+def toString(List):
+	return ''.join(List)
+
+def swap(a,b):
+	return b,a
 
 string = "a!!!b.c.d,e'f,ghi"
 print("Input string: " + string)
