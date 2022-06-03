cff1c390e76629c2a46cdc13fb05ec1979998d93
bikram.biswas <bikram.biswas@renewbuy.com>
2022-06-03 07:55:15 +0530

    Fix loop detection

--- 4_linked_list/4_detect_loop.py
+++ 4_linked_list/4_detect_loop.py
@@ -25,7 +25,7 @@ class LinkedList:
         fast_p = self.head
         while (slow_p and fast_p and fast_p.next):
             slow_p = slow_p.next
-            fast_p = fast_p.next
+            fast_p = fast_p.next.next
             if slow_p == fast_p:
                 print("Found Loop")
                 return
